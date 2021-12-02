cdc86a752043939cf649748eb4b4178203dc1de0
Ruben Sørensen <ruben.sorensen98@gmail.com>
2021-12-03 00:45:15 +0100

    Seperated function declarations and definitions to suitable header and source files

---
+++ src/helper_functions.cpp
@@ -0,0 +1,23 @@
+#include "helper_functions.h"
+
+std::vector<std::string> loadStringsToVector(const char* path)
+{
+  std::vector<std::string> data;
+  std::string line;
+  std::fstream file;
+  file.open(path);
+  while (getline(file, line))
+    data.push_back(line);
+  return data;
+}
+
+std::vector<int32_t> loadIntsToVector(const char* path)
+{
+  std::vector<int32_t> data;
+  std::string line;
+  std::fstream file;
+  file.open(path);
+  while (getline(file, line))
+    data.push_back(std::stoi(line));
+  return data;
+}
