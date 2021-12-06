4996ac34aaff9f81cc80c4f485aa5007391daaed
Ruben Sørensen <ruben.sorensen98@gmail.com>
2021-12-06 22:07:13 +0100

    Completed day 4

--- src/adv_4.cpp
+++ src/adv_4.cpp
@@ -1,5 +1,64 @@
 #include "adv_4.h"
 
+class Board
+{
+  public:
+  Board() = default;
+  void AddRow(std::vector<int32_t> row)
+  {
+    std::vector<std::pair<int32_t, bool>> vec;
+    for (auto num : row)
+      vec.push_back(std::make_pair(num, false));
+    m_Rows.push_back(vec);
+  }
+
+  void CheckNumber(int32_t number)
+  {
+    for (size_t i{0}; i < m_Rows.size(); ++i)
+    {
+      for (size_t j{0}; j < m_Rows[i].size(); ++j)
+        if (m_Rows[i][j].first == number)
+          m_Rows[i][j].second = true;
+    }
+  }
+
+  bool HasBingo()
+  {
+    bool bingoRow{false};
+    bool bingoColumn{false};
+    for (size_t i{0}; i < m_Rows.size(); ++i)
+    {
+      bool bingo{true};
+      for (size_t j{0}; j < m_Rows[i].size(); ++j)
+        if (!m_Rows[i][j].second)
+          bingo = {false};
+      bingoRow |= bingo;
+    }
+    for (size_t i{0}; i < m_Rows[0].size(); ++i)
+    {
+      bool bingo{true};
+      for (size_t j{0}; j < m_Rows.size(); j++)
+        if (!m_Rows[j][i].second)
+          bingo = {false};
+      bingoColumn |= bingo;
+    }
+    return bingoRow || bingoColumn;
+  }
+
+  int32_t Score(int32_t lastNum)
+  {
+    int32_t sum{0};
+    for (auto& r : m_Rows)
+      for (auto& n : r)
+        if (!n.second)
+          sum += n.first;
+    return sum * lastNum;
+  }
+
+  private:
+  std::vector<std::vector<std::pair<int32_t, bool>>> m_Rows;
+};
+
 std::vector<int32_t> lineToArray(std::string line, bool commaSeperated = false)
 {
   std::vector<int32_t> vec;
@@ -15,26 +74,6 @@ std::vector<int32_t> lineToArray(std::string line, bool commaSeperated = false)
   }
   return vec;
 }
-
-class Board
-{
-  public:
-  Board() = default;
-  void AddRow(std::vector<int32_t> row) { m_Rows.push_back(row); }
-  void ShowBoard()
-  {
-    for (auto& r : m_Rows)
-    {
-      for (auto& n : r)
-        std::cout << n << " ";
-      std::cout << std::endl;
-    }
-    std::cout << std::endl;
-  }
-
-  private:
-  std::vector<std::vector<int32_t>> m_Rows;
-};
 
 void loadData(std::vector<int32_t>& draws, std::vector<Board>& boards)
 {
@@ -62,17 +101,48 @@ void advent_of_code_4_part1()
   std::vector<int32_t> draws;
   std::vector<Board> boards;
   loadData(draws, boards);
-
-  std::cout << "Draws: " << std::endl;
-  for (auto& d : draws)
-    std::cout << d << " ";
-  std::cout << '\n' << std::endl;
-  std::cout << "Amount of boards: " << boards.size() << '\n' << std::endl;
-  for (size_t i{0}; i < boards.size(); ++i)
+  for (auto& draw : draws)
   {
-    std::cout << "Board " << i << ':' << std::endl;
-    boards[i].ShowBoard();
+    for (auto& board : boards)
+    {
+      board.CheckNumber(draw);
+      if (board.HasBingo())
+      {
+        std::cout << "Score: " << board.Score(draw) << std::endl;
+        return;
+      }
+    }
   }
 }
 
-void advent_of_code_4_part2() {}
+void advent_of_code_4_part2()
+{
+  std::vector<int32_t> draws;
+  std::vector<Board> boards;
+  loadData(draws, boards);
+  size_t drawIndex{0};
+  while (drawIndex < draws.size() && boards.size() > 1)
+  {
+    auto iter{boards.begin()};
+    while (iter != boards.end())
+    {
+      iter->CheckNumber(draws[drawIndex]);
+      if (iter->HasBingo())
+        boards.erase(iter);
+      else
+        ++iter;
+    }
+    ++drawIndex;
+  }
+  while (drawIndex < draws.size())
+  {
+    auto board{boards[0]};
+    board.CheckNumber(draws[drawIndex]);
+    if (board.HasBingo())
+    {
+      std::cout << "Score: " << board.Score(draws[drawIndex]);
+      return;
+    }
+    ++drawIndex;
+  }
+}
