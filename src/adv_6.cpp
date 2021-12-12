df717d43c2d0ea3d05f01ab6c9d878921edd9fd0
Ruben Sørensen <ruben.sorensen98@gmail.com>
2021-12-12 01:35:25 +0100

    Improved solutions for day 6

--- src/adv_6.cpp
+++ src/adv_6.cpp
@@ -12,42 +12,38 @@ void loadData(const char* filepath, int64_t* table)
     file.close();
 }
 
+void simulateDay(int64_t* table)
+{
+    int64_t temp = table[0];
+    for (size_t i = 0; i < 6; ++i)
+        table[i] = table[i + 1];
+    table[6] = temp + table[7];
+    table[7] = table[8];
+    table[8] = temp;
+}
+
+int64_t amountOfFish(int64_t* table)
+{
+    int64_t fish{0};
+    for (size_t i{0}; i < 9; ++i)
+        fish += table[i];
+    return fish;
+}
+
 void advent_of_code_6_part1()
 {
     int64_t table[9]{0};
     loadData("../data/adv_6.txt", table);
-
     for (size_t i{0}; i < 80; ++i)
-    {
-        int64_t temp = table[0];
-        for (size_t i = 0; i < 6; ++i)
-            table[i] = table[i + 1];
-        table[6] = temp + table[7];
-        table[7] = table[8];
-        table[8] = temp;
-    }
-    int64_t fish{0};
-    for (size_t i{0}; i < 9; ++i)
-        fish += table[i];
-    std::cout << "Fish: " << fish << std::endl;
+        simulateDay(table);
+    std::cout << "Fish: " << amountOfFish(table) << std::endl;
 }
 
 void advent_of_code_6_part2()
 {
     int64_t table[9]{0};
     loadData("../data/adv_6.txt", table);
-
     for (size_t i{0}; i < 256; ++i)
-    {
-        int64_t temp = table[0];
-        for (size_t i = 0; i < 6; ++i)
-            table[i] = table[i + 1];
-        table[6] = temp + table[7];
-        table[7] = table[8];
-        table[8] = temp;
-    }
-    int64_t fish{0};
-    for (size_t i{0}; i < 9; ++i)
-        fish += table[i];
-    std::cout << "Fish: " << fish << std::endl;
+        simulateDay(table);
+    std::cout << "Fish: " << amountOfFish(table) << std::endl;
 }
