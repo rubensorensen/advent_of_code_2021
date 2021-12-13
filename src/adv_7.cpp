22f94517bed9a4c6ef4f3f8304d47d736b33d0df
Ruben Sørensen <ruben.sorensen98@gmail.com>
2021-12-13 01:24:01 +0100

    Removed unneccesary code

--- src/adv_7.cpp
+++ src/adv_7.cpp
@@ -21,7 +21,7 @@ void advent_of_code_7_part1()
     auto n{crabs.begin()};
     auto m{n + (crabs.size() / 2)};
     std::nth_element(n, m, crabs.end()); // Place median value at m
-    for (auto n{crabs.begin()}; n != crabs.end(); ++n)
+    for (; n != crabs.end(); ++n)
         fuelCost += std::abs(*m - *n);
     std::cout << "Minimum fuel cost: " << fuelCost << std::endl;
 }
@@ -33,7 +33,6 @@ void advent_of_code_7_part2()
 
     int32_t fuelCost{0};
     int32_t avg{static_cast<int32_t>(std::accumulate(crabs.begin(), crabs.end(), 0) / crabs.size())};
-    std::cout << avg << std::endl;
     for (auto n{crabs.begin()}; n != crabs.end(); ++n)
         for (size_t i{1}; i <= std::abs(*n - avg); ++i)
             fuelCost += i;
